88378d4bc86a01fe4d71604875ec53b2beb4cb2d
MrSushi201 <santi@taweesamarn.com>
2023-11-26 14:51:18 +0100

    # ADD day3.sql

--- codewars/day2.sql
+++ codewars/day2.sql
@@ -7,7 +7,7 @@
 --# age_sum (sum of ages)
 
 --# NOTE: Your solution should use pure SQL. Ruby is used within the test cases to do the actual testing.
--- NOTE2: You need to use ALIAS for creating age_sum
+--# NOTE2: You need to use ALIAS for creating age_sum
 SELECT SUM(age) AS age_sum
 FROM people;
 
---
+++ codewars/day3.sql
@@ -0,0 +1,11 @@
+--# Greeting Grasshopper!
+--# Using only SQL, write a query that returns all rows in the custid, custname, and custstate columns from the customers table.
+--# Table Description for customers:
+--#     custid INTEGER 
+--#     custname STRING
+--#     custstate STRING
+--#     custard STRING 
+
+--# NOTE: Your solution should contain only SQL
+SELECT custid, custname, custstate
+FROM customers;
