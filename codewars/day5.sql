1e718c29af4fdeb2bbe085c7408b966d9eb7ec38
MrSushi201 <santi@taweesamarn.com>
2023-11-28 10:59:54 +0100

    # ADD day5.sql

---
+++ codewars/day5.sql
@@ -0,0 +1,9 @@
+--# Remove String Spaces
+--# Write a function that removes the spaces from the string, then return the resultant string. 
+--# Examples: Input --> Output
+--#           "8 j 8   mBliB8g  imjB8B8  jl  B" -> "8j8mBliB8gimjB8B8jlB"
+--#           "8aaaaa dddd r     " -> "8aaaaaddddr"
+
+--# Write your SQL statement here: 
+--# You are given a table 'nospace' with column 'x'.
+--# Return a table with column 'x' and your result in a column named 'res'.
