6ac9105a91dd8cc1f2294f6283ddbdeca1521e51
MrSushi201 <santi@taweesamarn.com>
2023-11-28 11:11:59 +0100

    # UPDATE day5.sql with another kata

--- codewars/day5.sql
+++ codewars/day5.sql
@@ -9,3 +9,21 @@
 --# Return a table with column 'x' and your result in a column named 'res'.
 SELECT x, REPLACE(x, ' ', '') AS res
 FROM nospace;
+
+--# Keep Hydrated!
+--# Nathan drinks 0.5 litres of water per hour of cycling. 
+--# You get tiven the time in hours and you need to return the number of litres Nathan will drink, rounded to the smallest value. 
+--# Examples: Input --> Output
+--#           hours = 3 --> liters = 1
+--#           hours = 6.7 --> liters = 3
+--#           hours = 11.8 --> liters = 5
+
+--# Input data is available from the table cycling, which has:
+--#     id
+--#     hours
+--# For each row, you have to return 3 columns:
+--#     id
+--#     hours
+--#     liters
+SELECT id, hours, FLOOR(0.5 * hours) AS liters
+FROM cycling;
