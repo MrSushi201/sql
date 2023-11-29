bd8faac028a55cc3a821d330e8b27740f0f0180f
MrSushi201 <santi@taweesamarn.com>
2023-11-29 11:08:24 +0100

    # UPDATE day6.sql with another kata

--- codewars/day6.sql
+++ codewars/day6.sql
@@ -15,3 +15,12 @@
 SELECT name, country
 FROM travelers
 WHERE country NOT IN ('Canada', 'Mexico', 'USA');
+
+--# Easy SQL: Rounding Decimals
+--# Given the following table 'decimals':
+--#     id
+--#     number1
+--#     number2
+--# Return a table with two columns (number1, number2), the value in number1 should be rounded down and the value in number2 should be rounded up.
+SELECT FLOOR(number1) as number1, CEILING(number2) as number2
+FROM decimals;
