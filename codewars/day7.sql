1d60fb9c15883cb4f17fe96f812804a2927d9f63
MrSushi201 <santi@taweesamarn.com>
2023-11-30 10:31:41 +0100

    # UPDATE day7.sql with another kata

--- codewars/day7.sql
+++ codewars/day7.sql
@@ -7,3 +7,21 @@
 --# You are given a table 'disemvowel' with column 'str', return a table with column 'str' and your result in a column named 'res'.
 SELECT str, TRANSLATE(str, 'aeiouAEIOU', '') AS res
 FROM disemvowel;
+
+--# SQL Basics - Position
+--# You have access to a table of monsters as follows:
+--# monsters schema
+--#     id
+--#     name
+--#     legs
+--#     arms
+--#     characteristics
+--# In each row, the characteristic column has a single comma. 
+--# Your job is to find it using position(). 
+--# You must return a table with the format as follows:
+--#     id
+--#     name
+--#     comma
+SELECT id, name, POSITION(',' IN characteristics) AS comma
+FROM monsters
+ORDER BY comma;
