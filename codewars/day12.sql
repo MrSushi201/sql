4b976d35d51798b4653a8e3d755c6076bfc125a6
MrSushi201 <santi@taweesamarn.com>
2023-12-05 15:36:05 +0100

    # UPDATE day12.sql with another kata

--- codewars/day12.sql
+++ codewars/day12.sql
@@ -12,3 +12,12 @@
 --# And the characteristics are the original strings in reverse.
 SELECT REPEAT(name, 3) AS name, REVERSE(characteristics) AS characteristics
 FROM monsters;
+
+--# Third Angle of a Triangle
+--# You are given two interior angles (in degrees) of a triangle. 
+--# Table: otherangle
+--#     a
+--#     b
+--# Return a table with a, b, and res
+SELECT a, b, (180 - (a + b)) AS res
+FROM otherangle;
